eeb2125eab63d69a18eca23845ad24ee72a5e818
Abdisheik3 <abdisheikh765@gmail.com>
2022-11-07 10:08:15 -0600

    slight touches

--- src/components/reviews/ReviewShow.js
+++ src/components/reviews/ReviewShow.js
@@ -59,12 +59,10 @@ const ShowReview = (props) => {
 
     return (
         <>
-
-            <Card className="m-2" >
-                <Card.Header onClick={() => navigate(`/profile/${review.username}`)}>Reviewed By: {review.username}</Card.Header>
-                <Card.Body>
-                    <h6>Score: { review.score }/5</h6>
-
+            {/* <Card style={cardCSS} className="m-2" >
+                <Card.Header style={cardHeader} onClick={() => navigate(`/profile/${review.username}`)}>Reviewed By: {review.username}</Card.Header>
+                <Card.Body style={cardBody}>
+                    <h6 style={boldText}>Score: { review.score }/5</h6>
                     { review.comment }
                 </Card.Body>
                 <Card.Footer style={cardFooter}>
@@ -93,7 +91,7 @@ const ShowReview = (props) => {
                         null
                     }
                 </Card.Footer>  
-                </Card>       
+                </Card>       */}
             <Card className="m-2" >
                 {review.username != user.username ?
                     <Card.Header>Reviewed By: {review.username} <Button className='btn-sm btn-success float-end'onClick={() => navigate(`/profile/${review.username}`)}>See {review.username}'s Profile</Button></Card.Header>
